78a4413681d5036d9bb66b419f775c57466dbd30
syednaqvi9512 <ALIhussnain"gmail.com>
2025-05-09 18:32:16 +0100

    Addi testing for clsLostItems

---
+++ Testing1/tstLostItems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstLostItems
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsLostItems AnLostItems = new clsLostItems();
+            Assert.IsNotNull(AnLostItems);
+
+        }
+    }
+}
