624d15ff7ea609cb7c0d0222d5f6c12cf6c812e9
zaid <Zaid@gmail.com>
2025-05-11 02:25:45 +0500

    Created the find method / practical 9 a

--- ClassLibrary/clsFoundItems.cs
+++ ClassLibrary/clsFoundItems.cs
@@ -4,10 +4,52 @@ namespace ClassLibrary
 {
     public class clsFoundItems
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Location { get; set; }
-        public DateTime DateFound { get; set; }
+        Int32 mId;
+        public int Id { 
+            get {
+                return mId;
+            } 
+            set {
+                mId = value;
+            } }
+
+        string mTitle;
+        public string Title { 
+            get {
+                return mTitle;
+            } 
+            set {
+                mTitle = value;
+            } }
+
+        string mLocation;
+        public string Location { 
+            get {
+                return mLocation;
+            } 
+            set {
+                mLocation = value;
+            } }
+
+        DateTime mDateFound;
+        public DateTime DateFound { 
+            get {
+                return mDateFound;
+            } 
+            set {
+                mDateFound = value;
+            } }
+
         public string IsReturned { get; set; }
+
+        public bool Find(int AddressId)
+        {
+            //set the private data members to the test data value
+            mId = 21;
+            mTitle = "Test Title";
+            mLocation = "Test Location";
+            mDateFound = Convert.ToDateTime("23/12/2022");
+            return true;
+        }
     }
 }
