16946c778d86c5ab1413582c41ac5e6e9679071c
Ali Hussnain <Alihussnain@gmail.com>
2025-05-11 01:18:39 +0500

    Created Properties for Table's items

--- ClassLibrary/clsFoundItems.cs
+++ ClassLibrary/clsFoundItems.cs
@@ -1,6 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsFoundItems
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime DateFound { get; set; }
+        public string IsReturned { get; set; }
     }
 }
