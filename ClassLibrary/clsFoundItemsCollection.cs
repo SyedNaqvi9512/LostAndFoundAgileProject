4ed44c9171ba3ef58575a4df53564eca457f877a
Zaid Ali <Zaid@gmail.com>
2025-05-18 04:36:36 +0500

    Created the half of pracctical 12

---
+++ ClassLibrary/clsFoundItemsCollection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsFoundItemsCollection
+    {
+        public clsFoundItemsCollection() { 
+            // constructor for the class
+            // create an instance of the found items class
+            clsFoundItems TestItems = new clsFoundItems();
+            TestItems.Title = "Test Title";
+            TestItems.Location = "Test Location";
+            TestItems.DateFound = DateTime.Now;
+            TestItems.IsReturned = "No";
+            // add the test item to the list
+            mFoundItemsList.Add(TestItems);
+            TestItems = new clsFoundItems();
+            TestItems.Title = "Test Title 2";
+            TestItems.Location = "Test Location 2";
+            TestItems.DateFound = DateTime.Now.Date;
+            TestItems.IsReturned = "Yes";
+            // add the test item to the list
+            mFoundItemsList.Add(TestItems);
+        }
+
+        List<clsFoundItems> mFoundItemsList = new List<clsFoundItems>();
+        public List<clsFoundItems> FoundItemsList { 
+            get {
+                return mFoundItemsList;
+
+            } 
+            set {
+                mFoundItemsList = value;
+
+            }
+        }
+        public int Count {
+            get {
+                return mFoundItemsList.Count;
+
+            }
+            set { 
+                //do nothing
+
+            }
+        }
+        public clsFoundItems ThisFoundItems { get; set; }
+    }
+}
