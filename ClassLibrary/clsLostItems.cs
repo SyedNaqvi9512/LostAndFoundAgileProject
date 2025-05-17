4e1c7da2f4d021d0e1c998c17681ce6953fdd38b
Ali Husnain <AliShah123@gmail.com>
2025-05-17 23:38:51 +0500

    Created the practical 10 (Added valid method)

--- ClassLibrary/clsLostItems.cs
+++ ClassLibrary/clsLostItems.cs
@@ -10,57 +10,78 @@ namespace ClassLibrary
         }
 
         private Int32 mId;
-        public int Id {
-            get {
+        public int Id
+        {
+            get
+            {
                 return mId;
             }
-            set {
+            set
+            {
                 mId = value;
-            } 
+            }
         }
         private DateTime mDate;
-        public DateTime DateLost {
-            get {
-                return mDate;   
+        public DateTime DateLost
+        {
+            get
+            {
+                return mDate;
             }
-            set {
+            set
+            {
                 mDate = value;
             }
         }
         private string mTitle;
-        public string Title {
-            get {
-              return mTitle;
+        public string Title
+        {
+            get
+            {
+                return mTitle;
             }
-            set { 
-            mTitle = value;
+            set
+            {
+                mTitle = value;
             }
         }
         private string mDescription;
-        public string Description {
-            get {
+        public string Description
+        {
+            get
+            {
                 return mDescription;
             }
-            set { 
-            mDescription = value;
+            set
+            {
+                mDescription = value;
             }
         }
 
         private string mLocation;
-        public string Location {
-            get {
+        public string Location
+        {
+            get
+            {
                 return mLocation;
             }
-            set{
+            set
+            {
                 mLocation = value;
             }
         }
         private string mIsClaimed;
-        public string IsClaimed { get {
+        public string IsClaimed
+        {
+            get
+            {
                 return mIsClaimed;
-                    } set {
+            }
+            set
+            {
                 mIsClaimed = value;
-                    } }
+            }
+        }
 
         public bool Find(int Id)
         {
@@ -90,6 +111,59 @@ namespace ClassLibrary
                 // Log the exception (optional)
                 throw new ApplicationException("An error occurred in the Find method: " + ex.Message);
             }
+        }
+
+        public string Valid(string title, string description, string location, string dateLost, string isClaimed)
+        {
+            string Error = "";
+            DateTime DateTemp;
+
+            if (title.Length == 0)
+            {
+                Error = Error + "Title cannot be blank. ";
+            }
+
+            if (title.Length > 50)
+            {
+                Error = Error + "Title cannot be more than 50 characters. ";
+            }
+            if (description.Length == 0) {
+                Error = Error + "Description cannot be blank. ";
+            } 
+            if (description.Length > 50) {
+                Error = Error + "Description cannot be more than 50 characters. ";
+            }
+
+            if (location.Length == 0) {
+                Error = Error + "Location cannot be blank. ";
+            }
+            if (location.Length > 50) {
+                Error = Error + "Location cannot be more than 50 characters. ";
+            }
+            if (isClaimed.Length == 0) {
+                Error = Error + "IsClaimed cannot be blank. ";
+            }
+            if (isClaimed.Length > 50) {
+                Error = Error + "IsClaimed cannot be more than 50 characters. ";
+            }
+            try
+            {
+                DateTemp = Convert.ToDateTime(dateLost);
+                if (DateTemp < DateTime.Now.Date)
+                {
+                    Error = Error + "Date cannot be in the past. ";
+                }
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    Error = Error + "Date cannot be in the future. ";
+                }
+            }
+            catch
+            {
+                Error = Error + "Invalid date format. ";
+            }
+
+            return Error;
         }
     }
 }
